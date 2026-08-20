95fe2028094d920e071b3e6d1e335780b5a840d4
Ben Park <152503119+benPark20@users.noreply.github.com>
2026-08-19 23:54:18 -0400

    Configure site and base URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Your main GitHub pages URL
+  site: 'https://slice-8738.github.io', 
+  // Your exact repository name, prefixed with a leading slash
+  base: '/slice-website', 
+});
