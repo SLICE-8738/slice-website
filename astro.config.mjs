511c806112bcfb2553e6486585a43433d55e41b3
Ben Park <152503119+benPark20@users.noreply.github.com>
2026-09-09 18:49:16 +0000

    update site URL in configuration to reflect new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // Your main GitHub pages URL
-  site: 'https://slice-8738.github.io', 
-  // Your exact repository name, prefixed with a leading slash
-  base: '/slice-website/',
+  site: 'https://slicerobotics.org',
   trailingSlash: 'always',
 });
